2b581ced33f9a23d55cc295f9aeea52ac28fa0fb
Kevin Lin <kwkevinlin@users.noreply.github.com>
2015-10-03 16:32:21 -0400

    Moved JS to become content scripts (js/content_script.js)

---
+++ js/content_script.js
@@ -0,0 +1,34 @@
+
+//When Document Ready
+$(document).ready(function() {
+
+    //Binding mouseup (selection) and control key
+    $(document).bind("mouseup", function(event) {
+
+        if (event.ctrlKey) {
+
+            var html = "";
+            if (typeof window.getSelection != "undefined") {
+                var sel = window.getSelection();
+                if (sel.rangeCount) {
+                    var container = document.createElement("div");
+                    for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                        container.appendChild(sel.getRangeAt(i).cloneContents());
+                    }
+                    html = container.innerHTML;
+                }
+            } else if (typeof document.selection != "undefined") {
+                if (document.selection.type == "Text") {
+                    html = document.selection.createRange().htmlText;
+                }
+            }
+            console.log(html);
+            //alert(html);
+        }
+
+    });
+
+}); //End document.ready()
+
+
+
