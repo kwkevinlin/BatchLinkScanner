69f2742802fc286fa00cd97ec63a4c9be4c8f66c
Kevin Lin <kwkevinlin@users.noreply.github.com>
2015-10-23 00:49:24 -0400

    Code cleanup

--- js/content_script.js
+++ js/content_script.js
@@ -1,7 +1,3 @@
-/* StackOverflow Question:
-    http://stackoverflow.com/questions/32928598/extract-url-from-html-text-but-if-url-only-shows-partial-like-secondpage-html
-*/
-
 //When Document Ready
 $(document).ready(function() {
 
@@ -41,61 +37,65 @@ function parseHTML(html) {
     if (urlArr === null) //End early if no URLs highlighted
         return;
 
-    //Retreiving settings from chrome storage
-    //Message passing to background.js required because content_script cant run a lot of chrome APIs
-    chrome.runtime.sendMessage({method: "getSettings"}, function(response) {
-        console.log(response.data);
-        console.log("This doesn't print either");
-    });
-
     //No lookahead in JavaScript?
     for (var i = 0; i < urlArr.length; i++) {
         urlArr[i] = urlArr[i].substring(6);
     }
     console.log(urlArr);
 
+    //Retreive domain.tld
     var domain = new URL(window.location.href).hostname; 
     //console.log(domain);
 
-    /*  Could mean two cases:
-        1. Linking to external page
-        2. Linking to same page, just without domain.tld
+    /* Retreiving settings from chrome storage
+       Message passing to background.js required because content_script cant run a lot of chrome APIs */
+    var urlToOpen;
+    chrome.runtime.sendMessage({method: "getSettings"}, function(response) {
+        console.log(response.data);
+        console.log("This doesn't print either");
 
-        http://stackoverflow.com/questions/2910946/test-if-links-are-external-with-jquery-javascript
-        var comp = new RegExp('^' + location.protocol + '//' + location.host);
-
-        Even easier?
-        1. / means relative
-        2. [blank] means replace domain.tld/r/test.html to domain.tld/r/[whatever]
-        3. # means anchor, append to current URL
-        4. Else, just go?
-
-        Current approach:
-        Possible issue: If current URL also contains domain.tld within URL
-    */
-
-    //Currently selecting last URL highlighted
-    if (urlArr[urlArr.length-1].indexOf(domain) != -1) { //URL not present. 
-
-        console.log("Internal");
-
-        //window.open(urlArr[urlArr.length-1]); //Opens hidden in new tab
-    } else {
-        console.log("External");
-        //window.open(urlArr[urlArr.length-1]); //Opens hidden in new tab
-    }
-
-
-    /* 
-        Corner case 1:
-        If #, anchor, so append to current URL
-            Or just ignore?
-    */
-
-
-
+        if (response.data == "boolFirst")
+            urlToOpen = urlArr[0];
+        else if (response.data == "boolLast")
+            urlToOpen = urlArr[urlArr.length-1];
+        else if (response.data == "boolAll")
+            urlToOpen = "boolAll"
+        else 
+            console.log("Error in urlToOpen");
     
+        /* Bad practice throwing everything into callback? */
+        
+        //Selecting last URL highlighted for testing
+        if (urlArr[urlArr.length-1].indexOf(domain) != -1) {
+
+            console.log("Internal");
+            //window.open(urlArr[urlArr.length-1]); //Opens hidden in new tab
+
+        } else {
+
+            console.log("External");
+            //window.open(urlArr[urlArr.length-1]); //Opens hidden in new tab
+
+        }
+
+    }); //End sendMessage
 }
 
 
+/*  Extracted URL could mean two cases:
+    1. Linking to external page
+    2. Linking to same page, just without domain.tld
 
+    http://stackoverflow.com/questions/2910946/test-if-links-are-external-with-jquery-javascript
+    var comp = new RegExp('^' + location.protocol + '//' + location.host);
+
+    Internal Linking Cases:
+    1. / means relative
+    2. [blank] means replace domain.tld/r/test.html to domain.tld/r/[whatever]
+    3. # means anchor, append to current URL
+    4. Else, just go?
+
+    Current approach:
+    Possible issue: If current URL also contains domain.tld within URL
+        Fix: Append location.protocol as well?
+*/
